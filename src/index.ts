67f2ca2763737be7c99bc31c0830bbd3b71a0b32
Allan Nienhuis <anienhuis@shotspotter.com>
2019-10-20 13:26:34 -0700

    tweaks to visibility, start of drag

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,50 @@
 /// <reference types="@types/googlemaps" 
+import * as Debug from 'debug';
+
+const debug = Debug('custom-marker');
 interface CustomMapOptions {
     map: google.maps.Map | null,
     element: HTMLElement,
-    position: google.maps.LatLng | null;
+    position: google.maps.LatLng;
 }
 export class CustomMarker extends google.maps.OverlayView {
 
-    public map: google.maps.Map | null;
     public element: HTMLElement;
-    public position: google.maps.LatLng | null;
+    public position: google.maps.LatLng;
     public projection: google.maps.MapCanvasProjection;
+
+    private mouseDownHandler: google.maps.MapsEventListener;
+    private mouseUpHandler: google.maps.MapsEventListener;
     constructor (opts: CustomMapOptions) {
         super();
 
         this.position = opts.position;
-        this.map = opts.map;
-        this.setMap(this.map);
+        this.setMap(opts.map);
         this.element = opts.element;
         this.element.style.position = 'absolute';
-        this.element.style.visibility = 'visible';
+        this.element.style.display = 'block';
         this.projection = this.getProjection();
 
 
-        console.log('custom marker');
+        debug('custom marker');
+
+        this.mouseDownHandler = google.maps.event.addDomListener(this.element, 'mousedown', (e) => {
+            if (!this.isVisible) {
+                return;
+            }
+            this.element.style.cursor = 'move';
+            const map = this.getMap();            
+            map.set('draggable', false);
+
+        });
+
+        this.mouseUpHandler = google.maps.event.addListener(this.getMap(), 'mouseup', (e) => {
+            this.element.style.cursor = 'default';
+            const map = this.getMap();            
+            map.set('draggable', true);
+            
+        });
+
     }
 
     public onAdd() {
@@ -31,13 +53,13 @@ export class CustomMarker extends google.maps.OverlayView {
     }
 
     public draw() {
-        if (!(this.map && this.position && this.element)) {
+        if (!(this.isVisible && this.position && this.getMap())) {
             return;
         }
         const point = this.projection.fromLatLngToDivPixel(this.position);
         // point.round(); // don't round. the layout engine is ok (better) with fractional/subpixel points
         this.element.style.transform = `translate(${point.x}px, ${point.y}px)`;
-        console.log(point);
+        debug('point', point);
 
         /*
         this.element.style.left = `${point.x}px`;
@@ -51,22 +73,21 @@ export class CustomMarker extends google.maps.OverlayView {
             return;
         }
         this.element.parentNode.removeChild(this.element);
+
+        google.maps.event.removeListener(this.mouseDownHandler);
+        google.maps.event.removeListener(this.mouseUpHandler);
     }
 
-    public hide() {
-        if (!this.element) {
-            return;
-        }
-        this.element.style.visibility = 'hidden';
-    }
-    public show() {
-
-        if (!this.element) {
-            return;
-        }
-        this.element.style.visibility = 'visible';
-    }
     public get isVisible(){
-        return !!(this.element && (this.element.style.visibility === 'visible'));
+        return this.element && (this.element.style.display === 'block');
+    }
+
+    public set isVisible(value) {
+        debug('value', value);
+        this.element.style.display = value ? 'block' : 'none';
+        if (value) {
+            // map may have moved since it was last visible.
+            this.draw();
+        }
     }
 }
